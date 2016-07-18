3cb74853394798c6b1c79d71488a02dc7a3c8176
zhiqizhang <zhiqizhang@moonton.com>
2016-07-18 11:05:22 +0800

    去除SDP

--- src/Utils/Log.lua
+++ src/Utils/Log.lua
@@ -20,17 +20,6 @@ local log_info =		3;
 local log_warn =		2;
 local log_error =		1;
 
-local print_to_file = cc.Application:getInstance():getTargetPlatform() == cc.PLATFORM_OS_WINDOWS
-local file = nil
-
-if print_to_file == true then
-    file = io.open("log.lua", "w");
-end
-
-local CACHE_LEN     =   5
-
-LogCache = Queue:new()
-
 
 --[[
 输出日志
@@ -39,19 +28,7 @@ LogCache = Queue:new()
 printLog(1, "222")
 ]]
 local function printLog(level, msg)
-    if LogCache:size() >= CACHE_LEN then
-        LogCache:pop()
-    end
-    LogCache:push(level .. " ".. msg)
-
-    --SystemTools 需要进行Lua绑定
-    --SystemTools:printLog(level .. " ".. msg);
-
-    if print_to_file == true and file ~= nil then
-        file:write(level .. " ".. msg.."\n");
-        file:flush()
-    end
---    print(level..""..msg)
+    print(level..""..msg)
 end
 
 function logDebug(...)
@@ -127,22 +104,6 @@ function logError(...)
     end
     printLog("[ERROR]", printResult);
 end
-
-PRINT_PROPERTY = cc.Application:getInstance():getTargetPlatform() == cc.PLATFORM_OS_WINDOWS
-function logProperty(...)
-    local arg = {...}
-    --过滤
-    if PRINT_PROPERTY ~= true then
-        return;
-    end
-    --输出
-    local printResult = "";
-    for i,v in ipairs(arg) do
-        printResult = printResult .. tostring(v) .. "  ";
-    end
-    printLog("[PROPERTY]", printResult);
-end
-
 
 --[[  打印table
 @lua_table  表对象
